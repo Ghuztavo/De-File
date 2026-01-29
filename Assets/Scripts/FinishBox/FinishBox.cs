511e52bf542bf3a0e6e812c7f85c828523ee5af4
Ghuztavo <ghuztavo2002@gmail.com>
2026-01-28 21:21:40 -0800

    Finish Box
    Box collider object that erases the particles when colliding with them, it doesn't pass anything yet.

---
+++ Assets/Scripts/FinishBox/FinishBox.cs
@@ -0,0 +1,21 @@
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class FinishBox : MonoBehaviour
+{
+    // There is no game manager yet
+    //[SerializeField] GameManager gameManager;
+    private int triggerCount = 0;
+
+    // Detect when particles enter the finish the box, destroy them and increase score
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+            triggerCount++;
+            Debug.Log("Particles finished: " + triggerCount);
+            //gameManager.IncreaseScore(1);
+        }
+    }
+}
