bd025f0260d38713a173f71e7fd03b2352d3b589
Ibrahim <ibrahimmiishaq@gmail.com>
2026-01-31 16:58:58 -0800

    MainMenu Added

---
+++ Assets/Scripts/UI Scripts/LevelSelectManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Level_One()
+    {
+        // Load Level One
+
+    }
+    public void Level_Two()
+    {
+        // Load Level Two
+    }
+    public void Level_Three()
+    {
+        // Load Level Three
+    }
+    public void Level_Four()
+    {
+
+    }
+    public void Level_Five()
+    {
+
+    }
+    public void Level_Six()
+    {
+
+    }
+    public void Level_Seven()
+    {
+
+    }
+    public void Level_Eight()
+    {
+
+    }
+
+    public void BackToMain()
+    {
+        // Load Main Menu Scene
+    }
+}
