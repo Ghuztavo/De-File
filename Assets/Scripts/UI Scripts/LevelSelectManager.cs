9ff8ee6069ffe76e1821ffe81efefff300c583f8
Ibrahim <ibrahimmiishaq@gmail.com>
2026-01-31 19:17:18 -0800

    Added Menu Button Functionality, Added Sharp Arrows

--- Assets/Scripts/UI Scripts/LevelSelectManager.cs
+++ Assets/Scripts/UI Scripts/LevelSelectManager.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class LevelSelectManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Level_One()
     {
-        // Load Level One
-
+        SceneManager.LoadScene(2);
     }
     public void Level_Two()
     {
-        // Load Level Two
+        SceneManager.LoadScene(3);
     }
     public void Level_Three()
     {
-        // Load Level Three
+        SceneManager.LoadScene(4);
     }
     public void Level_Four()
     {
-
+        SceneManager.LoadScene(5);
     }
     public void Level_Five()
     {
-
+        SceneManager.LoadScene(6);
     }
     public void Level_Six()
     {
-
+        SceneManager.LoadScene(7);
     }
     public void Level_Seven()
     {
-
+        SceneManager.LoadScene(8);
     }
     public void Level_Eight()
     {
-
+        SceneManager.LoadScene(9);
     }
 
     public void BackToMain()
     {
         // Load Main Menu Scene
+        SceneManager.LoadScene(0);
+    }
+
+    public void LevelSelect()
+    {
+        // Load Level Select Scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void NextLevel()
+    {
+        // Load the next level based on the current active scene's build index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
